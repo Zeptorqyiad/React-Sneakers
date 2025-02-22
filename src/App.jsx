6bf461d4de50442d53d67e8b456b9a83bd3cc653
Zeptorqyiad <Zeptorqyiad99@gmail.com>
2025-02-22 15:55:16 +0500

    Working with main pahe

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import LogoImage from './assets/image/logo.png'
-import CartSvg from './assets/image/cart.svg'
-import UserSvg from './assets/image/user.svg'
-import PlusImage from './assets/image/plus.svg'
-import Sneakers1 from './assets/sneakers/1.jpg'
-import Sneakers2 from './assets/sneakers/2.jpg'
-import Sneakers3 from './assets/sneakers/3.jpg'
-import Sneakers4 from './assets/sneakers/4.jpg'
+// import HeartLiked from './assets/image/liked.svg'
 
 const App = () => {
    return (
       <div className="wrapper clear">
+         <div className="overlay">
+            <div className="drawerBlock"></div>
+         </div>
          <header className="d-flex justify-between align-center p-40">
             <div className="d-flex align-center">
-               <img width={40} height={40} src={LogoImage} alt="LogoImage" />
+               <img
+                  width={40}
+                  height={40}
+                  src="/assets/image/logo.png"
+                  alt="LogoImage"
+               />
                <div>
                   <h3 className="text-uppercase">React Sneakers</h3>
                   <p className="opacity-5">Магазин лучших кроссовок</p>
@@ -20,22 +21,41 @@ const App = () => {
             </div>
             <ul className="d-flex">
                <li className="mr-30">
-                  <img width={18} height={18} src={CartSvg} alt="CartSvg" />
+                  <img
+                     width={18}
+                     height={18}
+                     src="/assets/image/cart.svg"
+                     alt="CartSvg"
+                  />
                   <span>1205 руб.</span>
                </li>
                <li>
-                  <img width={18} height={18} src={UserSvg} alt="UserSvg" />
+                  <img
+                     width={18}
+                     height={18}
+                     src="/assets/image/user.svg"
+                     alt="UserSvg"
+                  />
                </li>
             </ul>
          </header>
          <div className="content p-40">
-            <h1 className="mb-40">Все кроссовки</h1>
+            <div className="d-flex align-center justify-between mb-40">
+               <h1>Все кроссовки</h1>
+               <div className="search-block d-flex align-center">
+                  <img src="/assets/image/search.svg" alt="SearchSvg" />
+                  <input type="text" placeholder="Поиск..." />
+               </div>
+            </div>
             <div className="d-flex">
                <div className="card">
+                  <div className="favorite">
+                     <img src="/assets/image/unliked.svg" alt="HeartUnlinked" />
+                  </div>
                   <img
                      width={133}
                      height={112}
-                     src={Sneakers1}
+                     src="/assets/sneakers/1.jpg"
                      alt="Sneakers"
                   />
                   <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
@@ -48,17 +68,20 @@ const App = () => {
                         <img
                            width={11}
                            height={11}
-                           src={PlusImage}
+                           src="/assets/image/plus.svg"
                            alt="PlusImage"
                         />
                      </button>
                   </div>
                </div>
                <div className="card">
+                  <div className="favorite">
+                     <img src="/assets/image/unliked.svg" alt="HeartUnlinked" />
+                  </div>
                   <img
                      width={133}
                      height={112}
-                     src={Sneakers2}
+                     src="/assets/sneakers/2.jpg"
                      alt="Sneakers"
                   />
                   <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
@@ -71,17 +94,20 @@ const App = () => {
                         <img
                            width={11}
                            height={11}
-                           src={PlusImage}
+                           src="/assets/image/btn-plus.svg"
                            alt="PlusImage"
                         />
                      </button>
                   </div>
                </div>
                <div className="card">
+                  <div className="favorite">
+                     <img src="/assets/image/unliked.svg" alt="HeartUnlinked" />
+                  </div>
                   <img
                      width={133}
                      height={112}
-                     src={Sneakers3}
+                     src="/assets/sneakers/3.jpg"
                      alt="Sneakers"
                   />
                   <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
@@ -94,17 +120,20 @@ const App = () => {
                         <img
                            width={11}
                            height={11}
-                           src={PlusImage}
+                           src="/assets/image/btn-plus.svg"
                            alt="PlusImage"
                         />
                      </button>
                   </div>
                </div>
                <div className="card">
+                  <div className="favorite">
+                     <img src="/assets/image/unliked.svg" alt="HeartUnlinked" />
+                  </div>
                   <img
                      width={133}
                      height={112}
-                     src={Sneakers4}
+                     src="/assets/sneakers/4.jpg"
                      alt="Sneakers"
                   />
                   <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
@@ -117,7 +146,7 @@ const App = () => {
                         <img
                            width={11}
                            height={11}
-                           src={PlusImage}
+                           src="/assets/image/btn-plus.svg"
                            alt="PlusImage"
                         />
                      </button>
